3c501c66dbb87746fdcd5b1eb01e1b69b62a4b10
Sreeraj-palat <work.sreeraj@gmail.com>
2022-08-09 14:28:44 +0530

    update comments

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,14 @@ from .views import Teacher_RegisterView, Teacher_LoginView, Teacher_LogoutView, 
 
 
 urlpatterns = [
-    path('register/', Teacher_RegisterView.as_view()),
-    path('login/', Teacher_LoginView.as_view()),
-    path('logout/', Teacher_LogoutView.as_view()),
+    path('register/', Teacher_RegisterView.as_view()),#register teacher
+    path('login/', Teacher_LoginView.as_view()),#teacher login
+    path('logout/', Teacher_LogoutView.as_view()),#teacher logout
 
-    path('student/', StudentView.as_view()),
-    path('student_details/<int:id>/',Student_detailsView.as_view()),
+    path('student/', StudentView.as_view()),#create and view  student
+    path('student_details/<int:id>/',Student_detailsView.as_view()),#single student details
 
-    path('student_mark/', StudentMarkView.as_view()),
-    path('student_mark_details/<int:id>/',StudentMark_details.as_view()),
+    path('student_mark/', StudentMarkView.as_view()),#create and view marklist
+    path('student_mark_details/<int:id>/',StudentMark_details.as_view()),#single marklist details 
    
 ] 
--- api/views.py
+++ api/views.py
@@ -10,6 +10,7 @@ import jwt, datetime
 
 
 
+#Teacher registation view
 class Teacher_RegisterView(APIView):
     def post(self, request):
         serializer = AccountSerializer_teacher(data=request.data)
@@ -20,7 +21,7 @@ class Teacher_RegisterView(APIView):
 
 
 
-
+#Teacher login View
 class Teacher_LoginView(APIView):
     def post(self, request):
         email = request.data['email']
@@ -53,6 +54,7 @@ class Teacher_LoginView(APIView):
 
 
 
+#Teacher logout view
 class Teacher_LogoutView(APIView):
     def post(self, request):
         response = Response()
@@ -67,12 +69,15 @@ class Teacher_LogoutView(APIView):
 
 
 class StudentView(APIView):
+
+    #get the list of all students
     def get(self,request):
         student = Student.objects.all()
         serializer = StudentSerializer(student, many=True)
         return Response(serializer.data)
 
 
+    #create a new student
     def post(self,request):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
@@ -93,12 +98,16 @@ class Student_detailsView(APIView):
         except Student.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
+
+    #get single student details
     def get(self,request,id):
         student = self.get_object(id)
         serializer = StudentSerializer(student)
         return Response(serializer.data)
 
 
+
+    #update the single student details
     def put(self, request, id):
         student = self.get_object(id) 
         serializer = StudentSerializer(student, data=request.data)
@@ -107,6 +116,8 @@ class Student_detailsView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
+
+    #delete single student
     def delete(self, request, id):
         student = self.get_object(id) 
         student.delete()
@@ -116,12 +127,15 @@ class Student_detailsView(APIView):
 
 
 class StudentMarkView(APIView):
+
+    #get the mark list of all students
     def get(self,request):
         marks = StudentDetails.objects.all()
         serializer = StudentDetailsSerializer(marks,many=True)
         return Response(serializer.data)
 
 
+    #create a new mark list
     def post(self,request):
         serializer = StudentDetailsSerializer(data=request.data)
         if serializer.is_valid():
@@ -141,12 +155,14 @@ class StudentMark_details(APIView):
         except StudentDetails.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
+    #get one by one mark list
     def get(self,request,id):
         student = self.get_object(id)
         serializer = StudentDetailsSerializer(student)
         return Response(serializer.data)
 
 
+    #update the marklist
     def put(self, request, id):
         student = self.get_object(id) 
         serializer = StudentDetailsSerializer(student, data=request.data)
@@ -155,6 +171,8 @@ class StudentMark_details(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
+
+    #delete the mark list
     def delete(self, request, id):
         student = self.get_object(id) 
         student.delete()
